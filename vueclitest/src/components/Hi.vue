938f6b8167f1e73c0ddbb55d45cff4276affc5e0
6c5_100008589327_skysuhan <6c5_100008589327_skysuhan@git.cloud.tencent.com>
2019-04-15 17:07:27 +0800

    router1

---
+++ vueclitest/src/components/Hi.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>{{ msg }}</h2>
+    </div> 
+</template>
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      msg: 'Welcome to My Github Program,I am Coldcode'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
